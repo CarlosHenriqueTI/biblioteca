bd2d0387d9aed1e52084e7facf86d71765183b12
Carlos Henrique <carloshenriqueTI09@gmail.com>
2024-06-13 21:17:29 -0300

    primeiras linhas de codigo

--- livros.ts
+++ livros.ts
@@ -1,1 +1,33 @@
-//commit inicial da branch carlos
+export class livro {
+    id: number;
+    titulo: string;
+    autor: string;
+    editora: string;
+    ano: number;
+    genero: string;
+    sinopse: string;
+    imagem: string;
+    disponivel: boolean;
+    dataDevolucao: string;
+    dataEmprestimo: string;
+    idUsuario: number;
+    idBibliotecario: number;
+    idEmprestimo: number;
+
+constructor (id: number, titulo: string, autor: string, editora: string, ano: number, genero: string, sinopse: string, imagem: string, disponivel: boolean, dataDevolucao: string, dataEmprestimo: string, idUsuario: number, idBibliotecario: number, idEmprestimo: number){
+    this.id = id;
+    this.titulo = titulo;
+    this.autor = autor;
+    this.editora = editora;
+    this.ano = ano;
+    this.genero = genero;
+    this.sinopse = sinopse;
+    this.imagem = imagem;
+    this.disponivel = disponivel;
+    this.dataDevolucao = dataDevolucao;
+    this.dataEmprestimo = dataEmprestimo;
+    this.idUsuario = idUsuario;
+    this.idBibliotecario = idBibliotecario;
+    this.idEmprestimo = idEmprestimo;
+}
+}
